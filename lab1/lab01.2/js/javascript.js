84b4e675a58423184434c57ba3108042ff0b1936
Pchanh194 <anhphc.ptc@gmail.com>
2022-03-13 18:21:58 +1100

    Put lab1.1, lab1.2, lab1.3 into lab1

---
+++ lab1/lab01.2/js/javascript.js
@@ -0,0 +1,37 @@
+
+
+
+function init(){
+    const button2019 = document.getElementById("button1");
+    const button2021 = document.getElementById("button2");
+    const buttonboth = document.getElementById("button3");
+    //create button for each year
+    
+
+    button2019.addEventListener("click",()=>{
+        const image = document.getElementById("image");
+        image.src= "./image/chart2019.png";
+       
+    //add graphs
+        const caption=document.getElementById("caption");
+        caption.innerHTML= "Percentage of popular pet in 2019"
+    });
+
+    button2021.addEventListener("click",()=>{
+        const image = document.getElementById("image");
+        image.src= "./image/chart2021.png";
+        //add graphs
+        const caption=document.getElementById("caption");
+        caption.innerHTML= "Percentage of popular pet in 2021"
+        
+    });
+
+    buttonboth.addEventListener("click",()=>{
+        const image = document.getElementById("image");
+        //add graphs
+        image.src= "./image/bothyearchart.png";
+        const caption=document.getElementById("caption");
+        caption.innerHTML= "Percentage of popular pet in 2019 and 2021"
+    });
+}
+window.addEventListener("load",init)
