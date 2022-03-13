f742e9151355b7d7a740b5dbcc0bdfcf12674e32
kiennguyen2403 <63136586+kiennguyen2403@users.noreply.github.com>
2022-03-13 19:04:09 +0700

    add comments

--- lab2/lab02.1/data_binding.js
+++ lab2/lab02.1/data_binding.js
@@ -2,3 +2,4 @@ var dataset = [["Name: Arizona"," Density: 57.05"],["Name: Arkansas"," Density: 
                 ["Name: Connecticut"," Density:  739.1"],["Name: Delaware"," Density:  464.3"],
                 ["Name: District of Columbia"," Density:  10065"],["Name: Florida"," Density:  353.4"],["Name: Georgia"," Density:  169.5"],
                 ["Name: Hawaii"," Density:  214.1"]]
+//data used for drawing graphs
