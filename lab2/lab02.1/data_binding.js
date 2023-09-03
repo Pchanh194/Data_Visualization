9ce4944d56c92c0a518de3a563fd1ed346ab4e63
Pchanh194 <62698979+Pchanh194@users.noreply.github.com>
2023-09-04 02:32:51 +1000

    Delete lab2 directory

--- lab2/lab02.1/data_binding.js
+++
@@ -1,5 +0,0 @@
-var dataset = [["Name: Arizona"," Density: 57.05"],["Name: Arkansas"," Density: 56.43"],["Name: California"," Density:  241.7"],["Name: Colorado"," Density:  49.33"],
-                ["Name: Connecticut"," Density:  739.1"],["Name: Delaware"," Density:  464.3"],
-                ["Name: District of Columbia"," Density:  10065"],["Name: Florida"," Density:  353.4"],["Name: Georgia"," Density:  169.5"],
-                ["Name: Hawaii"," Density:  214.1"]]
-//data used for drawing graphs
